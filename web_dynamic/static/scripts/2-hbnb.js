1ca8acd5aa83d88ded65e6e9c60f5b8eaacb0866
byn3 <bryanleung92@gmail.com>
2019-02-22 23:18:04 +0000

    Copied 1 over to 2

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,21 @@
+$(document).ready(function () {
+  let dict = {};
+  $("input[type=checkbox]").click(function () {
+  console.log(this);
+    if (this.checked) {
+      dict[$(this).data('id')] = $(this).data('name');
+	} else {
+      delete dict[$(this).data('id')];
+	  if (dict.length === 0){
+	    $(".amenities h4").text("&nbsp;");
+	  }
+    }
+  $('.amenities h4').text(Object.values(dict));
+  });
+});
+
 $.get('http://0.0.0.0:5001/api/v1/status/').done(function (data) {
   if (data.status == 200) {
-    $('#api_status').addClass('available');
-  }
+      $('#api_status').addClass('available');
+	    }
 });

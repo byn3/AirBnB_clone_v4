9cc8fc3dfa3d52d3a4b92f5fef5edd72816d11b1
byn3 <bryanleung92@gmail.com>
2019-02-25 18:45:59 +0000

    Fetched all places. Need to display them

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -19,12 +19,33 @@ $(document).ready(function () {
       $('#api_status').addClass('available');
   }});
 
-  let url = 'http://0.0.0.0:5001/api/v1/places_search';
-  $.ajax(url, {
+  let url2 = 'http://0.0.0.0:5001/api/v1/places_search';
+  $.ajax({ url : url2,
     type : 'POST',
     contentType : 'application/json',
-    data : JSON.stringify({})
-  }
+    data : JSON.stringify({}),
+    success: function (data) {
+	for (let i = 0; i < data.length; i++) {
+	  console.log(data[i]);
+	  let place = data[i];
+	  $('.place').append('<article><div class="title"><h2>'
+	  + place.name + '</h2><div class="price_by_night">'
+	  + place.price_by_night
+	  + '</div></div><div class="information"><div class="max_guest">'
+	  + '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />'
+	  + place.max_guest + 'Guests </div><div class="number_rooms">'
+	  + '<i class="fa fa-bed fa-3x" aria-hidden="true"></i> <br/>'
+	  + place.number_rooms +'Bedrooms </div> <div class="number_bathrooms">' 
+	  + '<i class="fa fa-bath fa-3x" aria-hidden="true"></i><br/>'
+	  + place.number_bathrooms + 'Bathroom </div></div>'
+	  + '<div class="user"></div>'
+	  + '<div class="description">'
+	  + 
+	  + place.description + '</div></article>'
+	  );
+	}
+	}
+  });
   /*
     Loop into the result of the request and create an ARTICLE tag representing
     a Place in the SECTION.places. (you can remove the Owner tag in the place

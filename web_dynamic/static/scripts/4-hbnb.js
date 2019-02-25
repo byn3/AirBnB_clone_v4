6f6afaf026f2d7bf1d1ee7266c6fe9205c64d7ab
c-eng <ceng@cca.edu>
2019-02-25 20:41:57 +0000

    style: semistandard compliance

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,115 +1,77 @@
 let dict = {};
 $(document).ready(function () {
-  $("input[type=checkbox]").click(function () {
-  console.log(this);
+  $('input[type=checkbox]').click(function () {
+    console.log(this);
     if (this.checked) {
       dict[$(this).data('id')] = $(this).data('name');
-	} else {
+    } else {
       delete dict[$(this).data('id')];
-	  if (dict.length === 0){
-	    $(".amenities h4").text("&nbsp;");
-	  }
+      if (dict.length === 0) {
+        $('.amenities h4').text('&nbsp;');
+      }
     }
-  $('.amenities h4').text(Object.values(dict));
+    $('.amenities h4').text(Object.values(dict));
   });
-
   let url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
-  }});
-
+    }
+  });
   let url2 = 'http://0.0.0.0:5001/api/v1/places_search';
-  $.ajax({ url : url2,
-    type : 'POST',
-    contentType : 'application/json',
-    data : JSON.stringify({}),
+  $.ajax({ url: url2,
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
     success: function (data) {
-//	$('.place').empty();
-	for (let i = 0; i < data.length; i++) {
-	  let place = data[i];
-	  $('.places').append('<article><div class="title"><h2>'
-	  + place.name + '</h2><div class="price_by_night">'
-	  + place.price_by_night
-	  + '</div></div><div class="information"><div class="max_guest">'
-	  + '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />'
-	  + place.max_guest + 'Guests </div><div class="number_rooms">'
-	  + '<i class="fa fa-bed fa-3x" aria-hidden="true"></i> <br/>'
-	  + place.number_rooms +'Bedrooms </div> <div class="number_bathrooms">' 
-	  + '<i class="fa fa-bath fa-3x" aria-hidden="true"></i><br/>'
-	  + place.number_bathrooms + 'Bathroom </div></div>'
-	  + '<div class="user"></div>'
-	  + '<div class="description">'
-	  + place.description + '</div></article>'
-	  );
-	}
-	}
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+        $('.places').append('<article><div class="title"><h2>' +
+          place.name + '</h2><div class="price_by_night">' +
+          place.price_by_night +
+          '</div></div><div class="information"><div class="max_guest">' +
+          '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' +
+          place.max_guest + 'Guests </div><div class="number_rooms">' +
+          '<i class="fa fa-bed fa-3x" aria-hidden="true"></i> <br/>' +
+          place.number_rooms + 'Bedrooms </div> <div class="number_bathrooms">' +
+          '<i class="fa fa-bath fa-3x" aria-hidden="true"></i><br/>' +
+          place.number_bathrooms + 'Bathroom </div></div>' +
+          '<div class="user"></div>' +
+          '<div class="description">' +
+          place.description + '</div></article>'
+        );
+      }
+    }
   });
   $('button').click(function () {
-  $('SECTION.places article').remove();
-/*
-  let alist = [];
-    for (let j = 0; j < dict.length; j++)
-    {
-      alist.append(dict[j]);
-    }
-
-	console.log(alist);
-*/
-      $.ajax({ url : url2,
-        type : 'POST',
-        contentType : 'application/json',
-        data : JSON.stringify({"amenities" : Object.keys(dict)}),
-        success: function (data) {
-//          $('.places').empty();
-          for (let i = 0; i < data.length; i++) {
-            let place = data[i];
-            console.log(data[i]);
-            $('.places').append('<article><div class="title"><h2>'
-            + place.name + '</h2><div class="price_by_night">'
-            + place.price_by_night
-            + '</div></div><div class="information"><div class="max_guest">'
-            + '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />'
-            + place.max_guest + 'Guests </div><div class="number_rooms">'
-            + '<i class="fa fa-bed fa-3x" aria-hidden="true"></i> <br/>'
-            + place.number_rooms +'Bedrooms </div> <div class="number_bathrooms">' 
-            + '<i class="fa fa-bath fa-3x" aria-hidden="true"></i><br/>'
-            + place.number_bathrooms + 'Bathroom </div></div>'
-            + '<div class="user"></div>'
-            + '<div class="description">'
-            + place.description + '</div></article>'
+    $('SECTION.places article').remove();
+    $.ajax({ url: url2,
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ 'amenities': Object.keys(dict) }),
+      success: function (data) {
+        for (let i = 0; i < data.length; i++) {
+          let place = data[i];
+          console.log(data[i]);
+          $('.places').append('<article><div class="title"><h2>' +
+            place.name + '</h2><div class="price_by_night">' +
+            place.price_by_night +
+            '</div></div><div class="information"><div class="max_guest">' +
+            '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' +
+            place.max_guest + 'Guests </div><div class="number_rooms">' +
+            '<i class="fa fa-bed fa-3x" aria-hidden="true"></i> <br/>' +
+            place.number_rooms + 'Bedrooms </div> <div class="number_bathrooms">' +
+            '<i class="fa fa-bath fa-3x" aria-hidden="true"></i><br/>' +
+            place.number_bathrooms + 'Bathroom </div></div>' +
+            '<div class="user"></div>' +
+            '<div class="description">' +
+            place.description + '</div></article>'
           );
         }
-		if (data.length === 0) {
-		$('.places').append('<article>2 SPECIFIC. NO RESULTS</article>');
-		}
+        if (data.length === 0) {
+          $('.places').append('<article>2 SPECIFIC. NO RESULTS</article>');
         }
+      }
     });
   });
 });
-
-/*
-$.ajax({
-    type: "GET",
-    url: 'https://0.0.0.0:5001/api/v1/status/',
-    data: {},
-    success: console.log("poop")
-});
-*/
-/*
-$.ajax({
-    url: "https://0.0.0.0:5001/api/v1/status/",
-    data: {},
-    error: function(xhr, statusText, errorThrown){$('#api_status').addClass('.available');}
-});
-*/
-/*
-console.log("1");
-  $.ajax('https://0.0.0.0:5001/api/v1/status/').then(function (data) {
-  console.log("2");
-    if (data.status === "OK") {
-	console.log('IT WORKS');
-      $('#api_status').addClass('.available');
-	    }
-
-}); */

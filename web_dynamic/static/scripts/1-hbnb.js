31d313e0ed12458b9d7de2c827a66b91f1acc8ed
c-eng <ceng@cca.edu>
2019-02-25 20:41:35 +0000

    style: semistandard compliance

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,48 +1,15 @@
 $(document).ready(function () {
   let dict = {};
-  $("input[type=checkbox]").click(function () {
-  console.log(this);
+  $('input[type=checkbox]').click(function () {
+    console.log(this);
     if (this.checked) {
       dict[$(this).data('id')] = $(this).data('name');
-	} else {
+    } else {
       delete dict[$(this).data('id')];
-	  if (dict.length === 0){
-	    $(".amenities h4").text("&nbsp;");
-	  }
+      if (dict.length === 0) {
+        $('.amenities h4').text('&nbsp;');
+      }
     }
-  $('.amenities h4').text(Object.values(dict));
+    $('.amenities h4').text(Object.values(dict));
   });
 });
-	/*  
-	  
-	  if ($(this).is(":checked")) {
-	    list.push(this.data('name'));
-  	  }
-
-	  if ($(this).is(":checked") === false) {
-        let remove = this;
-
-		list = jQuery.grep(list, function(value) {
-		  return value != remove;
-		});
-		
-		// list.remove(this);
-	  }
-    });
-*/
-/*
-*/
- 
-  /*
-  $("input[type=checkbox]").click(function () {
-    if ($("input[type=checkbox]").checked) {
-      list.append(input[type=checkbox].data-name);
-
-	  // $(".amenities h4").text(Object.values(dict).join(', '));
-    }
-    else if ($("input[type=checkbox]").checked === false) {
-      list.remove(this.data-name);
-    }
-    $('h4').text(list);
-  }
-  */
